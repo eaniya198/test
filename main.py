48765d06437f69b4db86288e0c8737d37812bcbc
Dispiny <pjm1024cl@itnsa.cloud>
2023-05-29 21:47:28 +0900

    it

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import requests
-from bs4 import BeautifulSoup as bs
+from lxml import etree
 
-page = requests.get("https://eaniya198.github.io/test/index1.html")
-soup = bs(page.text, "html.parser")
+url = "https://eaniya198.github.io/test/index.html"  # 크롤링할 웹사이트 URL을 입력하세요.
 
-# elements = soup.select('div.esg-entry-content a > span')
+# 웹페이지 요청
+response = requests.get(url)
+html = response.text
 
-print(soup)
+# HTML 파싱
+parser = etree.HTMLParser()
+tree = etree.fromstring(html, parser)
 
-# for index, element in enumerate(elements, 1):
-# 		print("{} 번째 게시글의 제목: {}".format(index, element.text))
+# XPath를 사용하여 요소 추출
+div_elements = tree.xpath("/html/body/div")  # div 태그 요소들을 추출합니다.
+
+for i, div in enumerate(div_elements, start=1):
+    xpath_hcI = "./div[2]"  # hcD 클래스 요소의 위치를 XPath로 지정합니다.
+    hcI_elements = div.xpath(xpath_hcI)  # div 태그 내의 hcI 클래스 요소들을 추출합니다.
+
+    count = 0
+    for hcI in hcI_elements:
+        xpath_hls = "./div/div[contains(@class, 'hls') and contains(@class, 'ps0')]"
+        hls_elements = hcI.xpath(xpath_hls)  # hcI 클래스 요소 내의 hls ps0 클래스 요소들을 추출합니다.
+        count += len(hls_elements)
+
+    print(f"/html/body/div[{i}]: {count}개")
